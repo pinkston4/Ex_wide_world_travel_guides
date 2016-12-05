265da937afca9612f5684e38e4444a70c56a1049
john pinkston <pinkstonjack2@gmail.com>
2016-12-04 18:45:56 -0600

    finished final part of wwt

--- app/app.js
+++ app/app.js
@@ -1,3 +1,11 @@
 "use strict";
 
-var app = angular.module('TravelGuide', []);
+var app = angular.module('TravelGuide', ['ngRoute']);
+
+app.config( function($routeProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl: 'partials/guide-list.html',
+		controller: 'BookCtrl'
+	});
+});
